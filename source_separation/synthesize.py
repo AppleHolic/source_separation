c5e144178ec43a7f3b054f373f478c8e6a51a8d1
appleholic <choiilji@gmail.com>
2019-07-23 09:27:38 +0900

    add requirements, lowpass filter on synthesize

--- source_separation/synthesize.py
+++ source_separation/synthesize.py
@@ -7,24 +7,31 @@ from pytorch_sound import settings
 from pytorch_sound.utils.commons import get_loadable_checkpoint
 from pytorch_sound.models import build_model
 from pytorch_sound.utils.calculate import volume_norm_log_torch
+from pytorch_sound.utils.sound import lowpass
 
 
-def run(audio_file: str, out_path: str, model_name: str, pretrained_path: str):
+def run(audio_file: str, out_path: str, model_name: str, pretrained_path: str, is_norm: bool = True,
+        lowpass_freq: int = 0):
     wav, sr = librosa.load(audio_file, sr=settings.SAMPLE_RATE)
     if wav.dtype != np.float32:
         wav = wav.astype(np.float32)
+
+    if lowpass_freq:
+        wav = lowpass(wav, frequency=lowpass_freq)
 
     # load model
     print('Load model ...')
     model = build_model(model_name).cuda()
     chk = torch.load(pretrained_path)['model']
     model.load_state_dict(get_loadable_checkpoint(chk))
+    model.eval()
 
     # make tensor wav
     wav = torch.FloatTensor(wav).unsqueeze(0).cuda()
 
     # do volume norm
-    wav = volume_norm_log_torch(wav)
+    if is_norm:
+        wav = volume_norm_log_torch(wav)
 
     # inference
     print('Inference ...')
