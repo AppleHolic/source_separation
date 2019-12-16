e41f0529effbe831b2de5a263eeb72db62c10698
appleholic <choiilji@gmail.com>
2019-12-16 11:23:11 +0900

    Add musdb training, fix augmentation part

--- source_separation/dataset.py
+++ source_separation/dataset.py
@@ -14,8 +14,10 @@ from pytorch_sound.utils.sound import preemphasis
 class AugmentSpeechDataset(SpeechDataset):
 
     def __init__(self, meta_frame: MetaFrame, fix_len: int = 0, fix_shuffle: bool = False,
-                 skip_audio: bool = False, audio_mask: bool = False, extra_features: List[Tuple[str, Callable]] = None):
+                 skip_audio: bool = False, audio_mask: bool = False, extra_features: List[Tuple[str, Callable]] = None,
+                 is_audioset: bool = False):
         super().__init__(meta_frame, fix_len, fix_shuffle, skip_audio, audio_mask, extra_features)
+        self.is_audioset = is_audioset
 
     def __getitem__(self, idx: int) -> List[Any]:
         res = super().__getitem__(idx)
@@ -24,9 +26,10 @@ class AugmentSpeechDataset(SpeechDataset):
             res[0] = res[0] * -1
             res[1] = res[1] * -1
         # augmentation with audioset data / twice on three times
-        if np.random.randint(3):
-            rand_amp = np.random.rand() * 0.5 + 0.5
-            res[0] = augment(res[1], amp=rand_amp)
+        if self.is_audioset:
+            if np.random.randint(3):
+                rand_amp = np.random.rand() * 0.5 + 0.5
+                res[0] = augment(res[1], amp=rand_amp)
         # do volume augmentation
         rand_vol = np.random.rand() + 0.5  # 0.5 ~ 1.5
         res[0] = np.clip(res[0] * rand_vol, -1, 1)
@@ -39,7 +42,7 @@ class AugmentSpeechDataset(SpeechDataset):
 
 def get_datasets(meta_dir: str, batch_size: int, num_workers: int,
                  meta_cls: MetaFrame = VoiceBankMeta, fix_len: int = 0, skip_audio: bool = False,
-                 audio_mask: bool = False) -> Tuple[SpeechDataLoader, SpeechDataLoader]:
+                 audio_mask: bool = False, is_audioset: bool = False) -> Tuple[SpeechDataLoader, SpeechDataLoader]:
 
     assert os.path.isdir(meta_dir), '{} is not valid directory path!'
 
@@ -50,8 +53,12 @@ def get_datasets(meta_dir: str, batch_size: int, num_workers: int,
     valid_meta = meta_cls(os.path.join(meta_dir, valid_file))
 
     # create dataset
-    train_dataset = AugmentSpeechDataset(train_meta, fix_len=fix_len, skip_audio=skip_audio, audio_mask=audio_mask)
-    valid_dataset = AugmentSpeechDataset(valid_meta, fix_len=fix_len, skip_audio=skip_audio, audio_mask=audio_mask)
+    train_dataset = AugmentSpeechDataset(
+        train_meta, fix_len=fix_len, skip_audio=skip_audio, audio_mask=audio_mask, is_audioset=is_audioset
+    )
+    valid_dataset = AugmentSpeechDataset(
+        valid_meta, fix_len=fix_len, skip_audio=skip_audio, audio_mask=audio_mask, is_audioset=is_audioset
+    )
 
     # create data loader
     train_loader = SpeechDataLoader(train_dataset, batch_size=batch_size, is_bucket=False,
--- source_separation/train_jointly.py
+++ source_separation/train_jointly.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 from typing import Tuple
 from pytorch_sound.data.meta.dsd100 import DSD100Meta
+from pytorch_sound.data.meta.musdb18 import MUSDB18Meta
 from pytorch_sound.data.meta.voice_bank import VoiceBankMeta
 from pytorch_sound.models import build_model
 from torch.optim.lr_scheduler import MultiStepLR
@@ -11,12 +12,12 @@ from source_separation.dataset import get_concated_datasets
 from source_separation.trainer import Wave2WaveTrainer, LossMixingTrainer
 
 
-def main(vb_meta_dir: str, dsd_meta_dir: str, save_dir: str, save_prefix: str, pretrained_path: str = '',
+def main(vb_meta_dir: str, music_meta_dir: str, save_dir: str, save_prefix: str, pretrained_path: str = '',
          model_name: str = 'refine_unet_larger', batch_size: int = 128, num_workers: int = 16, fix_len: float = 2.,
          lr: float = 5e-4, betas: Tuple[float] = (0.5, 0.9), weight_decay: float = 0.0,
          max_step: int = 200000, valid_max_step: int = 50, save_interval: int = 1000, log_interval: int = 50,
          grad_clip: float = 0.0, grad_norm: float = 30.0,
-         milestones: Tuple[int] = None, gamma: float = 0.1, sample_rate: int = 44100,
+         milestones: Tuple[int] = None, gamma: float = 0.1, sample_rate: int = 44100, music_data_name: str = 'dsd100',
          mix_loss: bool = False):
 
     # create model
@@ -34,9 +35,17 @@ def main(vb_meta_dir: str, dsd_meta_dir: str, save_dir: str, save_prefix: str, p
     else:
         scheduler = None
 
+    # choose music source dataset
+    if music_data_name == 'musdb18':
+        MusicMeta = MUSDB18Meta
+    elif music_data_name == 'dsd100':
+        MusicMeta = DSD100Meta
+    else:
+        raise NotImplementedError(f'{music_data_name} is not implemented ! choose one in [\'musdb18\', \'dsd100\']')
+
     # make metas
-    meta_dir_list = [vb_meta_dir, dsd_meta_dir]
-    meta_cls_list = [VoiceBankMeta, DSD100Meta]
+    meta_dir_list = [vb_meta_dir, music_meta_dir]
+    meta_cls_list = [VoiceBankMeta, MusicMeta]
     meta_dir_list, meta_cls_list = map(list, zip(*[(d, cls) for d, cls in zip(meta_dir_list, meta_cls_list) if d]))
 
     train_loader, valid_loader = get_concated_datasets(
